9b7f3645d686a1750a316c70b42a5ea426c4e403
BotAnjinho <77682788+BotAnjinho@users.noreply.github.com>
2021-01-19 14:52:54 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,279 @@
+const help = (prefix) => {
+	return `
+╔─━━━━━✞━━━━━━╗
+➣ ஓீ፝͜͡ঔৣ͜͡ᗩᘉᒎ〇  𝜝𝛳͢𝑇 
+╚─━━━━━✞━━━━━━╝
+╠➥━━━━━━━━━━━━━━━━━━━━━━
+╠➸ Status: *「 Online ⏎」*
+╠➥*VERSAO:1.0* ⏎
+╠➥*Bot-Basico Privado* ⏎
+╠➥*CRIADOR:Anjo* ⏎
+╠➥*Wpp*: wa.me/5527997141457
+║━━━━━━━━━━━━━━━━━━━━━━
+
+╠➥🔰 Principais Comandos 🔰 
+
+Comando: '${prefix}sticker' ou '${prefix}stiker'
+Função: converter imagem / gif / vídeo em adesivo
+Como Usar: responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda 
+
+Comando: '${prefix}sticker nobg' ou '${prefix}stiker nobg'
+Função: converter imagem em adesivo removendo o fundo
+Como Usar: responder a imagem, ou enviar com legenda
+
+Comando: '${prefix}toimg'
+Função: converter adesivos em imagem
+Como Usar: responder a sticker 
+
+
+╠➥🔰 Comandos de Memes 🔰  
+
+Comando: '${prefix}meme'
+Função: imagens aleatórias de meme [english]
+
+Comando: '${prefix}memeindo'
+Função: imagens aleatórias de meme [indo]
+
+╠➥🔰 Comandos +18 🔰
+
+ATENÇÃO: Os servidores estão instaveis então caso não envie, mande o comando novamente
+
+Comando: '${prefix}nekoanime'
+Função: imagens aleatórias de neko
+
+Comando: '${prefix}randomkiss'
+Função: imagens aleatórias de beijo
+
+Comando: '${prefix}randomhug'
+Função: imagens aleatórias de abraço
+
+Comando: '${prefix}randomshota'
+Função: imagens aleatórias de shota
+
+Comando '${prefix}randomanime'
+Função: imagem aleatoria de anime
+
+Comando '${prefix}randomhentai'
+Função: imagem aleatoria de hentai
+
+Comando: '${prefix}nsfwblowjob'
+Função: gifs de blowjobs de hentai
+
+Comando: '${prefix}nsfwneko'
+Função: imagem de hentai neko
+
+Comando: '${prefix}nsfwtrap'
+Função: imagem de hentai trap
+
+Comando: '${prefix}nsfw'
+Função: Ativa o modo hentai no grupo
+Como Usar: '${prefix}nsfw 1' para ativar o modo nsfw e '${prefix}nsfw 0' para desativar o modo nsfw
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comando: '${prefix}pack'
+Função: Enviar um pacote de links +18
+Como Usar: Basta enviar o comando
+
+Obs: o comando .loli e .nsfwloli .shota foram removidos devido a o servidor nao estar funcionando
+
+╠➥🔰 Comandos de texto 🔰 
+
+ATENÇÃO: Os servidores estão instaveis então caso não envie, mande o comando novamente
+
+Comando: '${prefix}overtext'
+Função: converte texto em um nick de personagem do overwatch
+Como Usar: '${prefix}loltext [texto]'
+
+Comano: '${prefix}cstext'
+Função: converte texto em um nickname no CSGO
+Como Usar: '${prefix}cstext [texto]'
+
+Comando: '${prefix}warfacetext'
+Função: convert texto em um nickname no warface
+Como Usar: '${prefix}warfacetext [texto]'
+
+Comando: '${prefix}pubgtext'
+Função: comverte texto na logo da pubg
+Como Usar: '${prefix}pubgtext [pt1] | [pt2] '
+
+Comando: '${prefix}frase'
+Função: Envia uma frase filosofica
+
+Comando: '${prefix}textph'
+Função: converte o texto na logo da pornhub
+Como Usar: '${prefix}textph [pt1] | [pt2]'
+
+Comando: '${prefix}shadow'
+Função: converte o texto em uma imagem com sombras
+Como Usar: '${prefix}shadow [texto]'
+
+Comando: '${prefix}glowmetal'
+Função: converte o texto em uma imagem com efeito glowmetal
+Como Usar: '${prefix}glowmetal [texto]'
+
+Comando: '${prefix}harrytext'
+Função: converte o texto em uma imagem com efeito harry potter
+Como Usar: '${prefix}harrytext [texto]'
+
+Comando: '${prefix}woodblock'
+Função: converte o texto em uma imagem com efeito desenhado em madeira
+Como Usar: '${prefix}woodblock [texto]'
+
+Comando: '${prefix}ocean'
+Função: converte texto em uma imagem no oceano
+Como Usar: '${prefix}ocean [texto]'
+
+Comando: '${prefix}coffe'
+Função: converte texto em uma imagem no copo de café
+Como Usar: '${prefix}coffe [texto]'
+
+Comando: '${prefix}lovepaper'
+Função: converte texto em uma imagem em um coração de papel
+Como Usar: '${prefix}lovepaper [texto]'
+
+Comando: '${prefix}8bits'
+Função: converte texto em uma imagem em 8bits
+Como Usar: '${prefix}8bits [pt1] | [pt2]'
+
+Comando: '${prefix}bpaper'
+Função: converte texto em uma imagem em papel
+Como Usar: '${prefix}bpaper [texto]'
+
+Comando: '${prefix}traduz'
+Função: traduz seu texto em outra lingua
+Como Usar: '${prefix}traduz [la] [texto]'
+Exemplo: '${prefix}traduz pt hello my friends'
+
+Comando: '${prefix}idioma'
+Função: lista todos os códigos de idioma
+
+Comando: '${prefix}text3d'
+Função: converte o texto em imagem 3d
+Como Usar: '${prefix}text3d [pt1]'
+
+Comando: '${prefix}wolflogo'
+Função: converte o texto em imagem com o logo de um lobo
+Como Usar: '${prefix}wolflogo [pt1] | [pt2]'
+
+Comando: '${prefix}lionlogo'
+Função: converte o texto em imagem com o logo de um leao
+Como Usar: '${prefix}lionlogo [pt1] | [pt2]'
+
+Comando: '${prefix}glitch'
+Função: converte o texto em imagem com estilo glitch
+Como Usar: '${prefix}glitch [pt1] | [pt2]'
+
+Comando:'${prefix}ninjalogo'
+Função: converte o texto em imagem com o logo de um ninja
+Como Usar:'${prefix}ninjalogo [pt1] | [pt2]'
+
+Comando: '${prefix}neontec'
+Função: converte o texto em imagem com estilo neon tecnology
+Como Usar: '${prefix}neontec [pt1]'
+
+Comando:'${prefix}neonligth'
+Função: converte o texto em imagem com estilo neon ligth
+Como Usar:'${prefix}neonligth [pt1]'
+
+Comando:'${prefix}snow'
+Função: converte o texto em imagem com estilo em snow'
+Como Usar:'${prefix}snow [pt1]'
+
+Comando:'${prefix}blood'
+Função: converte o texto em imagem com estilo blood
+Como Usar:'${prefix}blood [pt1]'
+
+Comando:'${prefix}dwater'
+Função: converte o texto em imagem com estilo dropwater
+Como Usar:'${prefix}dropwater [pt1]'
+
+╠➥🔰 Outros Comandos 🔰 
+
+Comando: '${prefix}gimage'
+Função: Pesquisa no google imagens
+Como Usar: '${prefix}gimage [texto]'
+
+Comando: '${prefix}termux'
+Função: Mostra uma lista de comandos básicos do termux
+
+Comando: '${prefix}gtts'
+Função: converter texto em fala / áudio
+Como Usar: '${prefix}gtts [la] [texto]'
+Exemplo: '${prefix}gtts ja Oni-chan'
+
+Comando: '${prefix}signome'
+Função: Mostra o significado do seu nome
+Como usar:'${prefix}signome [nome]'
+Exemplo:'${prefix}signome ian'
+
+Comando: '${prefix}nethunter'
+Função: Mostra um tutorial de como ter um emulador de kali linux no seu termux
+
+Comando: '${prefix}db'
+Função: envia links de várias database de travazap
+
+Comando:'${prefix}wppim'
+Função: envia links de todos whatsapp imunes
+
+Comando: '${prefix}url2img'
+Função: tirar uma captura de tela na web
+Como Usar: *${prefix}url2img [tip] [url]*
+
+Comando: '${prefix}simi'
+Função: sua mensagem será respondida por simi
+Como Usar: '${prefix}simi [texto]'
+
+Comando: '${prefix}ocr'
+Função: pegue o texto na foto
+Como Usar: responder imagem ou enviar imagem com legenda 
+
+Comando: '${prefix}wait'
+Função: Pesquisa o anime na imagem [ O que anime é isso / aquilo ]
+Como Usar: responder imagem ou enviar imagem com legenda 
+
+Comando: '${prefix}setprefix'
+Função: substituir prefixo
+Como Usar: '${prefix}setprefix [text|optional]' 
+Exemplo: '${prefix}setprefix ?'
+
+╠➥🔰 Comandos de Grupos 🔰 
+
+Comandos: '${prefix}welcome'
+Função: Ativa o modo boas-vindas do grupo
+Como Usar: '${prefix}welcome 1' para ativar o modo welcome e '${prefix}welcome 0' para desativar o modo welcome
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comandos: '${prefix}linkgroup'
+Função: pegue o link do grupo
+Como Usar: basta enviar o comando
+Obs: só pode ser usado quando o bot se torna admin, e quem enviar o comando é admin! 
+
+Comandos: '${prefix}tagall'
+Função: marca todos os membros do grupo, incluindo administradores também
+Como Usar: basta enviar o comando
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comandos: '${prefix}simih'
+Função: ative o modo simi no grupo
+Como Usar: '${prefix}simih 1' para ativar o modo simih e '${prefix}simih 0' para desativar o modo simih
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+║ ═ ═〘𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾̧𝘼̃𝙊〙╗
+╠➥  ║▌│█║▌│ █║▌│█│║▌║
+╠➥  ║▌│█║▌│ █║▌│█│║▌║
+╠➥ Copyright ©️ ஓீ፝͜͡ঔৣ͜͡ᗩᘉᒎ〇《❍̷ℱՇ》
+║ 
+║ ══ ══ ══ ══ ══ ══ ══ 
+╠➥ Kibadores vão se fuder🐊
+╠➥ O brabo tem nome🔥
+║
+ =======*「𝘿𝙐𝙑𝙄𝘿𝘼」*=======
+➥ *Dono:*
+➥ *Wa.me/5527997141457*
+
+`
+}
+
+exports.help = help
+
+
