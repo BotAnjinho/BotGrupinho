20d32c99b7705eb034703db0dd1f45eb7f634b4b
BotAnjinho <77682788+BotAnjinho@users.noreply.github.com>
2021-01-25 23:40:02 -0300

    Add files via upload

---
+++ src/anjomenu.js
@@ -0,0 +1,12 @@
+const anjomenu = (prefix, pushname) => {
+    return `◪ *Comandos do ஓீ፝͜͡𝜝𝛳͢𝑇*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.anjomenu = anjomenu
