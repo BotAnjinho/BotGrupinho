60d01b17aaecc1c230364cf2d8b98cb6f96754b0
BotAnjinho <77682788+BotAnjinho@users.noreply.github.com>
2021-02-06 00:32:56 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,129 @@
+const help = (prefix) => {
+	return `	
+	IAE  COMO VC ESTA? ESPERO QUE ESTEJA BEM ESSE E O MENU DO BOT
+❉──────────❉
+
+   「 *DIABINHA BOT* 」
+
+❉──────────❉
+	
+╔════════════════════
+║╭──❉ *INFORMAÇÕES* ❉──
+║│➸ Prefix: 「  ${prefix}  」
+║│➸ Criador : *ANJO*
+║│➸ Numero do meu criador: https://wa.me/5527997141457
+║╰───────────    
+╠════════════════════
+║╭──❉ *STICKERS* ❉──
+║│➸ ${prefix}sticker OU ${prefix}stickergif
+║│Sobre : O comando  ${prefix}sticker OU ${prefix}stickergif faz figurinha
+║╰───────────
+║╭───────────
+║│➸${prefix}toimg
+║│Sobre : O comando ${prefix}toimg e para converter stickers  em fotos 
+║╰───────────
+║╭───────────
+║│➸${prefix}tomp3
+║│Sobre : o comando ${prefix}tomp3 e para converter videos em audio
+║╰───────────
+╠════════════════════
+║╭──❉ *MUSICA E AUDIO* ❉──
+║│➸${prefix}play
+║│Sobre : esse comando ${prefix}play e para abaixar musica
+║╰───────────
+║╭───────────
+║│➸${prefix}tts
+║│Sobre : esse comando e para o bot falar\n║│exemplo : ${prefix}tts pt pain bot
+║╰───────────
+╠════════════════════
+║╭──❉ *FOTOS E OUTRAS COISINHAS* ❉──
+║│➸${prefix}loli
+║│Sobre : esse ai e pros lolicon kkkkk
+║╰───────────
+║╭───────────
+║│➸${prefix}shota
+║│Sobre : as vezes vai mais e uma foto de um menino
+║╰───────────
+║╭───────────
+║│➸${prefix}imagem
+║│Sobre : esse pesquisa qual quer foto do pinterest
+║╰───────────
+║╭───────────
+║│➸${prefix}neko
+║│Sobre : esse e pros lolicon que gosta\n║│da oni-chan com rabinho e orelha de gato
+║╰───────────
+╠════════════════════
+║╭──❉ *OUTROS* ❉──
+║│➸ ${prefix}meulink
+║│Sobre : mando o wa.me/Seu numero
+║╰───────────
+║╭───────────
+║│➸ ${prefix}travazap
+║│Sobre : so pelo nome vcs ja sabem kkk
+║╰───────────
+║╭───────────
+║│➸ ${prefix}afk
+║│Sobre : esse comando e pra quele amigo corno que n deixa vc quieto
+║╰───────────
+║╭───────────
+║│➸ ${prefix}tempo
+║│Sobre : esse negocio ai nem sei pra que serve ksksksksks
+║╰───────────
+╠════════════════════
+║╭──❉ *SOBRE* ❉──
+║│➸ ${prefix}info
+║│Sobre : isso e para saber as info do bot
+║╰───────────
+║╭───────────
+║│➸ ${prefix}blocklist
+║│Sobre : lista de bloqueados do bot
+║╰───────────
+║╭───────────
+║│➸  ${prefix}ping
+║│Sobre : o ping do bot
+║╰───────────
+╠════════════════════
+║╭──❉ *ESSE AGORA E SO PRO TIO PAIN* ❉──
+║│➸  ${prefix}prefixo
+║│Sobre : muda o prefixo
+║╰───────────
+║╭───────────
+║│➸ ${prefix}block
+║│Sobre : bloquea a pessoa
+║╰───────────
+║╭───────────
+║│➸ ${prefix}bc
+║│Sobre : brodcast esamerda da ban no bot usa n
+║╰───────────
+║╭───────────
+║│➸ ${prefix}bcgc
+║│Sobre : brodcast diferenciado esamerda da ban no bot usa n
+║╰───────────
+║╭───────────
+║│➸ ${prefix}clone
+║│Sobre : clona a foto do amigo
+║╰───────────
+║╭───────────
+║│➸ ${prefix}clearall
+║│Sobre : limpa tudo do chat do bot
+║╰───────────
+╠════════════════════
+║╭──❉ *OUTROS MENU* ❉──
+║│➸ ${prefix}menuadmin
+║╰───────────
+║╭───────────
+║│➸ ${prefix}toinmenu
+║╰───────────
+║╭───────────
+║│➸ ${prefix}nsfwmenu
+║╰───────────
+║╭───────────
+║│➸ ${prefix}idiomas
+║╰───────────
+╠════════════════════
+║ BRABO REI DOS MENU
+╚════════════════════
+`
+}
+
+exports.help = help
---
+++ src/menuadmin.js
@@ -0,0 +1,98 @@
+const menuadmin = (prefix, pushname) => {
+ return `oiin usadores de zape zape aqui e o menu dos admins
+ _obs para o bot executar esses comandos e nescessario dar adm para o bot_
+ 
+╔════════════════════
+║╭──❉ *MENU ADMS* ❉── 
+║│➸${prefix}opengc
+║│Sobre : esse comando abre o grupo
+║╰───────────
+║╭───────────
+║│➸ ${prefix}closegc
+║│Sobre : esse comando fecha o grupo
+║╰───────────
+║╭───────────
+║│➸${prefix}promover
+║│Sobre : esse promove um membro comum em ademir
+║╰───────────
+║╭───────────
+║│➸${prefix}rebaixar
+║│Sobre : esse comando rebaixa um adm a membro comum
+║╰───────────
+║╭───────────
+║│➸${prefix}tagall
+║│Sobre : esse e pra chama geral kkk
+║╰───────────
+║╭───────────
+║│➸${prefix}tagall2
+║│Sobre : esse e pra chama geral kkk
+║╰───────────
+║╭───────────
+║│➸${prefix}tagall3
+║│Sobre : esse e pra chama geral kkk
+║╰───────────
+║╭───────────
+║│➸${prefix}tagall4
+║│Sobre : esse e pra chama geral kkk
+║╰───────────
+║╭───────────
+║│➸${prefix}tagall5
+║│Sobre : esse e pra chama geral kkk
+║╰───────────
+║╭───────────
+║│➸${prefix}add
+║│Sobre : esse adiciona um ctt no grupo\n║│exemplo : ${prefix}add 551199xxxxxxxx
+║╰───────────
+║╭───────────
+║│➸${prefix}kick
+║│Sobre : esse da o ban no cara chato\n║│exemplo : ${prefix}kick @tagdocomedia
+║╰───────────
+║╭───────────
+║│➸${prefix}listadmins
+║│Sobre : esse comando e o que mostra os ademir
+║╰───────────
+║╭───────────
+║│➸${prefix}linkgroup
+║│Sobre : esse e pra mandar o link do grupo
+║╰───────────
+║╭───────────
+║│➸${prefix}sair
+║│Sobre : esse e pra o bot sair do grupo
+║╰───────────
+║╭───────────
+║│➸${prefix}bemvindo
+║│Sobre : esse e para ativar a o bem vindo aos que entra por link\n║│exemplo : ${prefix}bemvindo 1 e para desativar e so botar o 0
+║╰───────────
+║╭───────────
+║│➸ ${prefix}nsfw
+║│Sobre : esse bota o nsfw 1 pra desativar 0 esse e o de hentai
+║╰───────────
+║╭───────────
+║│➸${prefix}leveling
+║│Sobre : esse e para ativar o level dos membros
+║╰───────────
+║╭───────────
+║│➸${prefix}level
+║│Sobre : esse e para ver o level 
+║╰───────────
+║╭───────────
+║│➸${prefix}delete
+║│Sobre : esse e para  o bot deletar o que ele mandou
+║╰───────────
+║╭───────────
+║│➸${prefix}simih
+║│Sobre : nunca ative isso namoral chato pra caramba
+║╰───────────
+║╭───────────
+║│➸${prefix}ownergroup
+║│Sobre : esse e para saber quem e o dono do grupo
+║╰───────────
+╠════════════════════
+║ DIABINHA BOT
+╚════════════════════
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
