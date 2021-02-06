2de34e255221743a15f2c8d03300362c82d7685f
BotAnjinho <77682788+BotAnjinho@users.noreply.github.com>
2021-02-06 00:48:42 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,31 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do pain*
+╔════════════════════
+║╭──❉ *MENU DA DIABINHA* ❉── 
+║│➸${prefix}prefixo
+║│Sobre : esse muda o prefixo
+║╰───────────
+║╭───────────
+║│➸${prefix}block
+║│Sobre : esse da block
+║╰───────────
+║╭───────────
+║│➸${prefix}bc
+║│Sobre : esse da ban no numero usa naum
+║╰───────────
+║╭───────────
+║│➸${prefix}bcgc
+║│Sobre : esse da ban no numero de um geito diferente
+║╰───────────
+║╭───────────
+║│➸${prefix}clearall
+║│Sobre : esse limpa todas conversa do bot
+║╰───────────
+╠════════════════════
+║ BOT DIABINHA
+╚════════════════════
+`
+
+}
+
+exports.toinmenu = toinmenu
